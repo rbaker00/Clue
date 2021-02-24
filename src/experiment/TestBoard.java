8bb342f973ea1fdc319c5cc08e2ff62424d327d6
rbaker00 <reed.baker00@gmail.com>
2021-02-23 18:03:42 -0700

    Added all required methods and instance variables to TestBoardCell

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -1,5 +1,19 @@
 package experiment;
 
-public class TestBoard {
+import java.util.*;
 
+public class TestBoard {
+	Set<TestBoardCell> ans;
+	public TestBoard() {
+		
+	}
+	void calcTargets(TestBoardCell start, int pathlength) {
+		
+	}
+	Set<TestBoardCell> getTarget() {
+		return ans;
+	}
+	TestBoardCell getCell(int row, int col) {
+		
+	}
 }
--- src/experiment/TestBoardCell.java
+++ src/experiment/TestBoardCell.java
@@ -1,5 +1,38 @@
 package experiment;
 
-public class TestBoardCell {
+import java.util.HashSet;
+import java.util.Set;
 
+public class TestBoardCell {
+	private int row;
+	private int column;
+	private Set<TestBoardCell> adjacents;
+	private boolean room;
+	private boolean occupied;
+	
+	public TestBoardCell(int row, int column) {
+		super();
+		this.row = row;
+		this.column = column;
+		adjacents = new HashSet<TestBoardCell>();
+	}
+	
+	void addAdjacency(TestBoardCell cell) {
+		adjacents.add(cell);
+	}
+	Set<TestBoardCell> getAdjList() {
+		return adjacents;
+	}
+	void setRoom(boolean room) {
+		this.room = room;
+	}
+	boolean getRoom() {
+		return room;
+	}
+	void setOccupied(boolean occupied) {
+		this.occupied = occupied;
+	}
+	boolean getOccupied() {
+		return occupied;
+	}
 }
