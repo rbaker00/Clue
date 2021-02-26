7b1d237a893d2df1084636728bca5b45e09576fb
hpurdum <henrypurdum@gmail.com>
2021-02-26 10:40:55 -0800

    BoardCell file complete

--- src/clueGame/BoardCell.java
+++ src/clueGame/BoardCell.java
@@ -1,5 +1,15 @@
 package clueGame;
 
-public class BoardCell {
+import java.util.Set;
 
+public class BoardCell {
+	private int row, col;
+	private char initial;
+	private DoorDirection doorDirection;
+	private Boolean roomLabel, roomCenter;
+	private char secretPassage;
+	private Set<BoardCell> adjList;
+	public void addAdj(BoardCell adj) {
+		adjList.add(adj);
+	}
 }
