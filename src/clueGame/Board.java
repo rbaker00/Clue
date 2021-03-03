15ba55a5aa4e4f8f732f1e841dd3ed9d8d91990c
hpurdum <henrypurdum@gmail.com>
2021-03-02 17:52:44 -0800

    refactoring setup methods

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -45,26 +45,29 @@ public class Board {
 		try {
 			myReader = new Scanner(setup);
 			roomMap = new HashMap<Character, Room>();
-			while (myReader.hasNextLine()) {
-				
-		        String[] data = myReader.nextLine().split(", ");
-		        if(data.length > 3) { //thrown if line is not a comment and is too long
-		        	myReader.close();
-		        	throw new BadConfigFormatException();
-		        }
-		        if (data[0].equals("Room") || data[0].equals("Space")) { //puts the data of the room in the appropriate data structures
-			        rooms.add(new Room(data[1]));
-			        roomMap.put(data[2].charAt(0), rooms.get(rooms.size()-1));
-		        }
-		        else if (!data[0].substring(0, 2).equals("//")){ //thrown if a line does not contain Room or Space and is not a comment
-		        	myReader.close();
-		        	throw new BadConfigFormatException();
-		        }
-		    }
+			setupRoomsAndRoomMap(myReader);
 		    myReader.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 			return;
+		}
+	}
+	private void setupRoomsAndRoomMap(Scanner myReader) {
+		while (myReader.hasNextLine()) {
+			
+		    String[] data = myReader.nextLine().split(", ");
+		    if(data.length > 3 && !data[0].substring(0, 2).equals("//")) { //thrown if line is not a comment and is too long
+		    	myReader.close();
+		    	throw new BadConfigFormatException();
+		    }
+		    if (data[0].equals("Room") || data[0].equals("Space")) { //puts the data of the room in the appropriate data structures
+		        rooms.add(new Room(data[1]));
+		        roomMap.put(data[2].charAt(0), rooms.get(rooms.size()-1));
+		    }
+		    else if (!data[0].substring(0, 2).equals("//")){ //thrown if a line does not contain Room or Space and is not a comment
+		    	myReader.close();
+		    	throw new BadConfigFormatException();
+		    }
 		}
 	}
 	//Loads the file that stores the board
@@ -81,71 +84,74 @@ public class Board {
 			String[] theLine = lines.get(0).split(",");
 			numColumns = theLine.length;
 			grid = new BoardCell[numRows][numColumns];
-			for (int row = 0; row < numRows; row++) {
-				if (theLine == null) {
-					theLine = lines.get(row).split(",");
-				}
-				if(theLine.length != numColumns) {
-					throw new BadConfigFormatException("Board layout file does not have the same number of columns in every row.");
-				}
-				for (int col = 0; col < numColumns; col++) {
-					char initial = theLine[col].charAt(0);
-					if(!roomMap.containsKey(initial)) { //checks that the room is valid
-						throw new BadConfigFormatException("Board layout refers to room that is not in the setup file.");
-					}
-					DoorDirection doorDirection = DoorDirection.NONE;
-					boolean roomLabel = false;
-					boolean roomCenter = false;
-					char secretPassage = ' ';
-					if (theLine[col].length() > 1) {
-						if(theLine[col].length() > 2) {
-							throw new BadConfigFormatException("Too many characters in Board Cell from layout file");
-						}
-						switch(theLine[col].charAt(1)) { //handles when a cell has some sort of modifier after the initial
-						case '#':
-							roomLabel = true;
-							break;
-						case '*':
-							roomCenter = true;
-							break;
-						case '<':
-							doorDirection = DoorDirection.LEFT;
-							break;
-						case '^':
-							doorDirection = DoorDirection.UP;
-							break;
-						case '>':
-							doorDirection = DoorDirection.RIGHT;
-							break;
-						case 'v':
-							doorDirection = DoorDirection.DOWN;
-							break;
-						default:
-							secretPassage = theLine[col].charAt(1);
-							if(!roomMap.containsKey(secretPassage)) {
-								throw new BadConfigFormatException("Board layout refers to room that is not in the setup file.");
-							}
-							break;
-						}
-					}
-					else if(theLine[col].length() != 1) {
-						throw new BadConfigFormatException("Empty cell in layout file.");
-					}
-					grid[row][col] = new BoardCell(row, col, initial, doorDirection, roomLabel, roomCenter, secretPassage);
-					if (roomLabel) {
-						roomMap.get(initial).setLabelCell(grid[row][col]);
-					}
-					else if (roomCenter) {
-						roomMap.get(initial).setCenterCell(grid[row][col]);
-					}
-				}
-				theLine = null;
-			}
+			setupGrid(lines, theLine);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 			return;
 		}
 		
+	}
+	private void setupGrid(ArrayList<String> lines, String[] theLine) {
+		for (int row = 0; row < numRows; row++) {
+			if (theLine == null) {
+				theLine = lines.get(row).split(",");
+			}
+			if(theLine.length != numColumns) {
+				throw new BadConfigFormatException("Board layout file does not have the same number of columns in every row.");
+			}
+			for (int col = 0; col < numColumns; col++) {
+				char initial = theLine[col].charAt(0);
+				if(!roomMap.containsKey(initial)) { //checks that the room is valid
+					throw new BadConfigFormatException("Board layout refers to room that is not in the setup file.");
+				}
+				DoorDirection doorDirection = DoorDirection.NONE;
+				boolean roomLabel = false;
+				boolean roomCenter = false;
+				char secretPassage = ' ';
+				if (theLine[col].length() > 1) {
+					if(theLine[col].length() > 2) {
+						throw new BadConfigFormatException("Too many characters in Board Cell from layout file");
+					}
+					switch(theLine[col].charAt(1)) { //handles when a cell has some sort of modifier after the initial
+					case '#':
+						roomLabel = true;
+						break;
+					case '*':
+						roomCenter = true;
+						break;
+					case '<':
+						doorDirection = DoorDirection.LEFT;
+						break;
+					case '^':
+						doorDirection = DoorDirection.UP;
+						break;
+					case '>':
+						doorDirection = DoorDirection.RIGHT;
+						break;
+					case 'v':
+						doorDirection = DoorDirection.DOWN;
+						break;
+					default:
+						secretPassage = theLine[col].charAt(1);
+						if(!roomMap.containsKey(secretPassage)) {
+							throw new BadConfigFormatException("Board layout refers to room that is not in the setup file.");
+						}
+						break;
+					}
+				}
+				else if(theLine[col].length() != 1) {
+					throw new BadConfigFormatException("Empty cell in layout file.");
+				}
+				grid[row][col] = new BoardCell(row, col, initial, doorDirection, roomLabel, roomCenter, secretPassage);
+				if (roomLabel) {
+					roomMap.get(initial).setLabelCell(grid[row][col]);
+				}
+				else if (roomCenter) {
+					roomMap.get(initial).setCenterCell(grid[row][col]);
+				}
+			}
+			theLine = null;
+		}
 	}
 	public void setConfigFiles(String layoutConfigFile, String setupConfigFile) {
 		this.setupConfigFile = "data/" + setupConfigFile;
