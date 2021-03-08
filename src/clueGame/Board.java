a0e2de99ce64b85425db6392832643774d4d98df
hpurdum <henrypurdum@gmail.com>
2021-03-08 14:06:10 -0800

    all tests pass

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -199,16 +199,6 @@ public class Board {
 				continue;
 			}
 			visited.add(adjCell);
-//			if(thisCell.isRoomCenter()) {
-//				for(BoardCell door : roomMap.get(thisCell.getInitial()).getDoors()) {
-//					if(!door.getOccupied()) {
-//						targets.add(door);
-//					}
-//				}
-//				if(roomMap.get(thisCell.getInitial()).getSecretPassage()!=null) {
-//					targets.add(roomMap.get(thisCell.getSecretPassage()).getCenterCell());
-//				}
-//			}
 			if(numSteps==1 || (adjCell.getInitial()!='W' && adjCell.getInitial()!='X')) {
 				targets.add(adjCell);
 			} else {
