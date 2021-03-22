3db6c5734a7a0e9265d3ac4e30166e4328eafa9c
rbaker00 <reed.baker00@gmail.com>
2021-03-22 15:06:08 -0600

    Added test for testing that players are loaded in properly

--- src/clueGame/Player.java
+++ src/clueGame/Player.java
@@ -1,16 +1,23 @@
 package clueGame;
 
+import java.util.ArrayList;
+
 public abstract class Player {
 	String name;
 	String color;
 	char playerType;
 	BoardCell startingLocation;
+	ArrayList<Card> hand;
 	Player(String name, String color) {
 		super();
 		this.name = name;
 		this.color = color;
+		hand = new ArrayList<Card>();
 	}
 	public void updateHand(Card card) {
 		return;
+	}
+	public ArrayList<Card> getHand() {
+		return hand;
 	}
 }
--- src/tests/GameSetupTests.java
+++ src/tests/GameSetupTests.java
@@ -1,5 +1,35 @@
 package tests;
 
-public class GameSetupTests {
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import clueGame.Board;
+import clueGame.Player;
+
+public class GameSetupTests {
+	private static Board board;
+	
+	@BeforeAll
+	public static void setUp() {
+		// Board is singleton, get the only instance
+		board = Board.getInstance();
+		// set the file names to use my config files
+		board.setConfigFiles("board.csv", "ClueSetup.txt");		
+		// Initialize will load config files 
+		board.initialize();
+	}
+	@Test
+	public void testPeople() {
+		Player[] people = board.getPlayers();
+		assertEquals(6, people.length);
+		assertTrue("Reed Baker".equals(people[0]));
+		assertTrue("Henry Purdum".equals(people[1]));
+		assertTrue("Stephen Strange".equals(people[2]));
+		assertTrue("Hippocrates".equals(people[3]));
+		assertTrue("Martha Ballard".equals(people[4]));
+		assertTrue("Mary Seacole".equals(people[5]));
+	}
 }
