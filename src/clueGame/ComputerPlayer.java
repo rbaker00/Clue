8bef6f22db975b859fecf8e6ca42af2777e2fa2e
rbaker00 <reed.baker00@gmail.com>
2021-03-23 17:38:44 -0600

    Created stubs for createSolution and selectTargets

--- src/clueGame/ComputerPlayer.java
+++ src/clueGame/ComputerPlayer.java
@@ -6,4 +6,10 @@ public class ComputerPlayer extends Player {
 	ComputerPlayer(String name, Color color, int row, int col) {
 		super(name, color, row, col);
 	}
+	public Solution createSolution() {
+		return null;
+	}
+	public BoardCell selectTargets() {
+		return null;
+	}
 }
