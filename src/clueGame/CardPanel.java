d3214bb484a711ddb8556543e360e485753c53f0
rbaker00 <reed.baker00@gmail.com>
2021-04-02 17:42:34 -0600

    Switched panels to use box layout

--- src/clueGame/CardPanel.java
+++ src/clueGame/CardPanel.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import java.awt.FlowLayout;
 
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -20,16 +21,36 @@ public class CardPanel extends JPanel {
 	JPanel weaponSeen;
 	public CardPanel() {
 		super(new BorderLayout());
-		peopleHand = new JPanel(new BorderLayout());
-		peopleSeen = new JPanel(new BorderLayout());
-		roomHand = new JPanel(new BorderLayout());
-		roomSeen = new JPanel(new BorderLayout());
-		weaponHand = new JPanel(new BorderLayout());
-		weaponSeen = new JPanel(new BorderLayout());
+		peopleHand = new JPanel();
+		peopleHand.setLayout(new BoxLayout(peopleHand, BoxLayout.Y_AXIS));
+		peopleHand.add(new JLabel("In Hand:"));
+		peopleSeen = new JPanel();
+		peopleSeen.setLayout(new BoxLayout(peopleSeen, BoxLayout.Y_AXIS));
+		peopleSeen.add(new JLabel("Seen:"));
+		roomHand = new JPanel();
+		roomHand.setLayout(new BoxLayout(roomHand, BoxLayout.Y_AXIS));
+		roomHand.add(new JLabel("In Hand:"));
+		roomSeen = new JPanel();
+		roomSeen.setLayout(new BoxLayout(roomSeen, BoxLayout.Y_AXIS));
+		roomSeen.add(new JLabel("Seen:"));
+		weaponHand = new JPanel();
+		weaponHand.setLayout(new BoxLayout(weaponHand, BoxLayout.Y_AXIS));
+		weaponHand.add(new JLabel("In Hand:"));
+		weaponSeen = new JPanel();
+		weaponSeen.setLayout(new BoxLayout(weaponSeen, BoxLayout.Y_AXIS));
+		weaponSeen.add(new JLabel("Seen:"));
 	}
 	private void createUI() {
-		JPanel all = new JPanel(new BorderLayout());
+		JPanel all = new JPanel();
+		all.setLayout(new BoxLayout(all, BoxLayout.Y_AXIS));
 		all.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black), "Known Cards"));
+		
+		JPanel people = new JPanel();
+		people.setLayout(new BoxLayout(people, BoxLayout.Y_AXIS));
+		people.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black), "People"));
+		people.add(peopleHand);
+		people.add(peopleSeen);
+		all.add(people);
 		add(all, BorderLayout.CENTER);
 	}
 	public static void main(String[] args) {
